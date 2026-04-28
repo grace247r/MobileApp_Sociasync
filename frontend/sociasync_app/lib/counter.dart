c94d970d462bdb31ffb7bd66f5d003d1be0a42c6
vincentchou-pm <23502410025@student.prasetiyamulya.ac.id>
2026-04-28 10:18:18 +0700

    counter test

---
+++ frontend/sociasync_app/lib/counter.dart
@@ -0,0 +1,13 @@
+
+class Counter {
+  int value = 0;
+
+  void increment() {
+    value++;
+  }
+
+  void decrement() {
+    value--;
+  }
+
+}
---
+++ frontend/sociasync_app/test/test_counter.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sociasync_app/counter.dart';
+
+void main() {
+  test('Counter initial value test', () {
+    final counter = Counter();
+    expect(counter.value, 0);
+  });
+  
+  test('Counter increments test', () {
+    final counter = Counter();
+    counter.increment();
+    expect(counter.value, 1);
+  });
+
+  test('Counter decrements test', () {
+    final counter = Counter();
+    counter.decrement();
+    expect(counter.value, -1);
+  });
+}
